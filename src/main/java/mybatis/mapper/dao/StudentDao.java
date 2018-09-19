91b1777bc7136421d933c53c61de2eaed0738fd0
CAICAISKY <37895043+CAICAISKY@users.noreply.github.com>
2018-09-19 16:51:23 +0800

    Mybatis增删改查

--- src/main/java/mybatis/mapper/dao/StudentDao.java
+++ src/main/java/mybatis/mapper/dao/StudentDao.java
@@ -4,13 +4,19 @@ import mybatis.entity.Student;
 import mybatis.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @author Schuyler
  */
 public class StudentDao {
-
+    private SqlSession sqlSession = MybatisUtil.getSqlSession();
+    /**
+     * 增加一个学生
+     * @param student
+     */
     public void addStudent(Student student) {
-            SqlSession sqlSession = MybatisUtil.getSqlSession();
         try {
             sqlSession.insert("StudentMapper.addStudent", student);
             sqlSession.commit();
@@ -20,6 +26,74 @@ public class StudentDao {
         } finally {
             MybatisUtil.close();
         }
+    }
 
+    /**
+     * 获取对应条件的学生列表
+     * @param student
+     */
+    public void getStudentList(Student student) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", student.getName());
+        map.put("age", student.getAge());
+        try {
+            List<Student> students = sqlSession.selectList("StudentMapper.getStudentList", student);
+            for (Student studentResult : students) {
+                System.out.println(studentResult);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            MybatisUtil.close();
+        }
+    }
+
+    /**
+     * 更新
+     * @param student
+     */
+    public void updateStudent(Student student) {
+        try {
+            sqlSession.update("StudentMapper.updateStudent", student);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            MybatisUtil.close();
+        }
+    }
+
+    /**
+     * 删除指定id的student
+     * @param ids
+     */
+    public void deleteStudents(List<Integer> ids) {
+        try {
+            sqlSession.delete("StudentMapper.deleteStudents", ids);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            MybatisUtil.close();
+        }
+    }
+
+    /**
+     * 批量插入
+     * @param students
+     */
+    public void addStudents(List<Student> students) {
+        try {
+            sqlSession.insert("StudentMapper.addStudents", students);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            MybatisUtil.close();
+        }
     }
 }
--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -2,14 +2,55 @@ import mybatis.entity.Student;
 import mybatis.mapper.dao.StudentDao;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MybatisTest {
+    private StudentDao studentDao = new StudentDao();
 
     @Test
-    public void testMybatis () {
+    public void testMybatisAdd() {
         Student student = new Student();
-        student.setAge(20);
-        student.setName("caicaicai");
-        StudentDao studentDao = new StudentDao();
+        student.setAge(333);
+        student.setName("addCai");
         studentDao.addStudent(student);
+    }
+
+    @Test
+    public void testMybatisSelect () {
+        Student student = new Student();
+      /*  student.setAge(20);
+        student.setName("caicaicai");*/
+        studentDao.getStudentList(student);
+    }
+
+    @Test
+    public void testMybatisUpdate() {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("SchuylerChanged");
+        studentDao.updateStudent(student);
+    }
+
+    @Test
+    public void testMybatisDelete() {
+        ArrayList<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(3);
+        studentDao.deleteStudents(ids);
+    }
+
+    @Test
+    public void testMybatisAddAll() {
+        List<Student> students = new ArrayList<>();
+        Student studentOne = new Student();
+        studentOne.setName("caiOne");
+        studentOne.setAge(111);
+        Student studentTwo = new Student();
+        studentTwo.setName("caiTwo");
+        studentTwo.setAge(222);
+        students.add(studentOne);
+        students.add(studentTwo);
+        studentDao.addStudents(students);
     }
 }
